ef01e1c57784de1d6ac11243ccea9786a25a094b
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-02 12:15:09 +0100

    init commit

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# in memory data store
+
+todos = []
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to the To-Do List API!"}), 200
+
+
